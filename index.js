667461b3f596fc0f5b3f776e924e59986425938f
Veronica-rhody-219 <ngocdiep219@gmail.com>
2022-10-30 17:01:56 +0700

    feat: update all files and folders

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // add active class to active navLinks
 const currentLocation = location.pathname;
 
-const navLinks = document.querySelectorAll("nav a");
+const navLinks = document.querySelectorAll(".act a");
 navLinks.forEach((link) => {
   if (link.href.includes(`${currentLocation}`)) {
     link.classList.add("active");
