751db730e6ecfd8da3e9de4d6fece6a96cf65c4b
Veronica-rhody-219 <ngocdiep219@gmail.com>
2022-11-05 08:21:58 +0700

    feat: update screen size 1024px

--- index.js
+++ index.js
@@ -1,10 +1,9 @@
 // add active class to active navLinks
-const currentLocation = location.pathname;
-console.log(currentLocation);
+const currentLocation = window.location.href;
 
 const navLinks = document.querySelectorAll(".act a");
 navLinks.forEach((link) => {
-  if (link.href.includes(`${currentLocation}`)) {
+  if (link.href === currentLocation) {
     link.classList.add("active");
   }
 });
